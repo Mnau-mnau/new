afcfae7e611591e92f4bc2bf4999a0adbe704d67
Natalie Mikešová <xpercentme@gmail.com>
2017-03-08 10:19:47 +0100

    footer

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>THIS IS A FILE</title>
-    <style>
+    <style> 
         .some{
             background-color:pink;
             width:400px;
@@ -19,5 +19,9 @@
     <div class="some">
         <h3>So here we are, in a box</h3>
     </div>
+
+    <footer>
+    &copy; <?php echo date('Y'); ?> The spectacular me
+</footer>
 </body>
 </html>
